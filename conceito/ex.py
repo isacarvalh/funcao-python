ba48088115694db9d33ea06c9b43b3181f8160e0
Isa <belagames012@gmail.com>
2023-11-10 08:25:35 -0300

    Aula 10/11/2023

---
+++ conceito/ex.py
@@ -0,0 +1,7 @@
+def dobro(x): # definindo a função - nome(parametro, parametro, ...)
+    dobro = x*2 # definir o que ela executa
+    return dobro # o que ela retorna
+
+x = int (input("Informe um número: "))
+
+print(dobro(x)) # chamando a função - nome_da_funcao (parametro)
