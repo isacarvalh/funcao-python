8dac60790ecee86afad4a64fcb1b1c8cb2b3e564
isa <belagames012@gmail.com>
2023-11-13 07:56:40 -0300

    primeiro commit

---
+++ lista/1.py
@@ -0,0 +1,5 @@
+salario =  float (input("Qual salário base? "))
+bonus =  float (input("Qual é a porcentagem do seu bônus? "))
+desconto =  float (input("Qual seu desconto salárial? "))
+
+def 
---
+++ lista/2.py
@@ -0,0 +1,5 @@
+def nome (nome):
+    nome = input("Digite seu nome: ")
+    return nome
+
+print("Seja bem-vindo", nome(nome),"!")
