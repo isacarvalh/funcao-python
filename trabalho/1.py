3eb2cc2d6f9e853b1894c1b564d7facc5028519d
isa <belagames012@gmail.com>
2023-11-13 07:41:20 -0300

    trabalho

---
+++ trabalho/1.py
@@ -0,0 +1,10 @@
+choice = input("Deseja fazer uma operação? [ s ou n ] ")
+
+while choice == 's':
+    def soma (a):
+        soma = a+b 
+        return soma 
+    a = float (input("Digite o a: "))
+    b = float (input("Digite o b: "))
+    print("Resultado: ", soma(a))
+ 
