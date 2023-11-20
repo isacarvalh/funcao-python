e8f7715c025d5178cbe452a43011af5215b18d8b
Isabela <135141032+isacarvalh@users.noreply.github.com>
2023-11-20 20:59:07 -0300

    replit

--- trabalho/1.py
+++ trabalho/1.py
@@ -1,10 +1,32 @@
-choice = input("Deseja fazer uma operação? [ s ou n ] ")
+choice = input("Deseja fazer uma operação? [ s ou n ]: ")
 
-while choice == 's':
+if choice == 's':
+  op = input("Qual operacao (+, -, *, /): ")
+elif op == '+':
     def soma (a):
         soma = a+b 
-        return soma 
+        return soma
     a = float (input("Digite o a: "))
     b = float (input("Digite o b: "))
     print("Resultado: ", soma(a))
- 
+    choice = input("Deseja fazer uma operação? [ s ou n ]: ")
+    if choice == 's':
+     op = input("Qual operacao (+, -, *, /): ")
+     if op == '-':
+      def subtracao (a):
+        subtracao = a-b 
+        return subtracao
+    a = float (input("Digite o a: "))
+    b = float (input("Digite o b: "))
+    print("Resultado: ", subtracao(a))
+    choice = input("Deseja fazer uma operação? [ s ou n ]: ")
+    if choice == 's':
+     op = input("Qual operacao (+, -, *, /): ")
+    elif op == '*':
+      def multiplicacao (a):
+        multiplicacao = a*b 
+        return multiplicacao
+    a = float (input("Digite o a: "))
+    b = float (input("Digite o b: "))
+    print("Resultado: ", multiplicacao(a))
+
