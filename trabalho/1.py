e02688ba8d1eaaba2199bdec56d138b1f8622e4d
Isabela <135141032+isacarvalh@users.noreply.github.com>
2023-11-27 07:29:18 -0300

    cabo

--- trabalho/1.py
+++ trabalho/1.py
@@ -13,58 +13,37 @@ l_div = []
 
 choice = input("Deseja fazer uma operação? [ s ou n ]: ")
 
-if choice == 's':
+while choice == 's':
     op = input("Qual operacao (+, -, *, /): ")
+    a = float(input("Digite o a: "))
+    b = float(input("Digite o b: "))
     if op == '+':
         def soma(a, b):
             soma = a+b
             return soma
-        a = float(input("Digite o a: "))
-        b = float(input("Digite o b: "))
         print(f"Resultado: {a} + {b} = ", soma(a, b))
         l_soma.append(soma(a, b))
-
-choice = input("Deseja fazer uma operação? [ s ou n ]: ")
-
-if choice == 's':
-    op = input("Qual operacao (+, -, *, /): ")
-    if op == '-':
+    elif op == '-':
         def subtracao(a, b):
             subtracao= a-b
             return subtracao
-    a = float(input("Digite o a: "))
-    b = float(input("Digite o b: "))
-    print(f"Resultado: {a} - {b} = ", subtracao(a, b))
-    l_sub.append(subtracao(a, b))
-
-choice = input("Deseja fazer uma operação? [ s ou n ]: ")
-
-if choice == 's':
-    op = input("Qual operacao (+, -, *, /): ")
-    if op == '*':
+        print(f"Resultado: {a} - {b} = ", subtracao(a, b))
+        l_sub.append(subtracao(a, b))
+    elif op == '*':
         def multiplicacao(a, b):
             multiplicacao = a*b
             return multiplicacao
-    a = float(input("Digite o a: "))
-    b = float(input("Digite o b: "))
-    print(f"Resultado: {a} x {b} = ", multiplicacao(a, b))
-    l_mult.append(multiplicacao(a, b))
-
-choice = input("Deseja fazer uma operação? [ s ou n ]: ")
-
-if choice == 's':
-    op = input("Qual operacao (+, -, *, /): ")
-    if op == '/':
+        print(f"Resultado: {a} x {b} = ", multiplicacao(a, b))
+        l_mult.append(multiplicacao(a, b))
+    elif op == '/':
         def divisao(a, b):
             divisao = a/b
             return divisao
-    a = float(input("Digite o a: "))
-    b = float(input("Digite o b: "))
-    print(f"Resultado: {a} / {b} = ", divisao(a, b))
-    l_div.append(divisao(a, b))
+        print(f"Resultado: {a} / {b} = ", divisao(a, b))
+        l_div.append(divisao(a, b))
+    choice = input("Deseja fazer uma operação? [ s ou n ]: ")
 
-elif choice == 'n':
-    print("Calculadora desligada!")
+print("Calculadora desligada!")
 
 print()
 print("Histórico de resultados: ")
@@ -72,3 +51,4 @@ print("Adições efetuadas:", l_soma)
 print("Subtrações efetuadas:", l_sub)
 print("Multiplicações efetuadas:", l_mult)
 print("Divisões efetuadas:", l_div)
+ 
